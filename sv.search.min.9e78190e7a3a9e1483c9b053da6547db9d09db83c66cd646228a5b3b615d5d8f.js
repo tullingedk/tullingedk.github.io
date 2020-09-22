760f2ae6b54b451ff6cfe2ba84a188968801578a
Vilhelm Prytz (via Travis CI) <vilhelm@prytznet.se>
2020-09-22 19:08:55 +0000

    Deploy tgdk.se to github.com/tullingedk/tullingedk.github.io.git:master

--- sv.search.min.9e78190e7a3a9e1483c9b053da6547db9d09db83c66cd646228a5b3b615d5d8f.js
+++ sv.search.min.9e78190e7a3a9e1483c9b053da6547db9d09db83c66cd646228a5b3b615d5d8f.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/sv.search-data.min.9afaf2679cb43ba30d73ca43b1c3959d9fe1ca0592e2de188af2d8e71c60d706.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/sv.search-data.min.cfb2964490424a4f446edce8f0a2f01a3856756ca76e4801c5dafe75853ed3c9.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
